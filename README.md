024f70e6991bfa350837edfe1db5a50f4d69c6a5
WPCom100 <landrystrickland@gmail.com>
2021-03-05 20:01:13 -0600

    update to v1.1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# FS19_WPToolKit
+# WPToolKit - Farming Simulator 2019 Mod
+
+A collection of tools for Farming Simulator 2019, used on a private game server.
+
+Changelog 
+V1.1 -  Added heavy weight lifting.
+        Night cycle skip function now resumes on game relog.
+V1.0 -  Created
--- WPToolKit.lua
+++ WPToolKit.lua
@@ -6,75 +6,87 @@
   -- @date 03/05/2021
   --
   
-WPToolKit = {};
+WPToolKit = {}
 
-addModEventListener(WPToolKit);
+addModEventListener(WPToolKit)
 
 
 function WPToolKit:loadMap(name)
-    
-    WPToolKit.isRunning = false
-    Player.MAX_PICKABLE_OBJECT_MASS = 999;
+  WPToolKit.events = {}
+  Player.registerActionEvents = Utils.appendedFunction(Player.registerActionEvents, WPToolKit.registerActionEvents);
+  Player.updateActionEvents = Utils.appendedFunction(Player.updateActionEvents, WPToolKit.registerActionEvents);
+  Player.removeActionEvents = Utils.appendedFunction(Player.removeActionEvents, WPToolKit.removeActionEvents);
+  Enterable.onRegisterActionEvents = Utils.appendedFunction(Enterable.onRegisterActionEvents, WPToolKit.registerActionEvents);
 
+  WPToolKit.speedForwardIsRunning = false
+  WPToolKit.strongIsEnabled = false
+  WPToolKit.DEFAULT_PICKABLE_MASS = Player.MAX_PICKABLE_OBJECT_MASS
+end;
+
+function WPToolKit:removeActionEvents()
+	WPToolKit.events = {}
+end;
+
+function WPToolKit:registerActionEvents()
+	WPToolKit.events = {}
+	local result, eventName = InputBinding.registerActionEvent(g_inputBinding, "STRONGFARMER", self, WPToolKit.toggleStrongFarmer, false, true, false, true)
+
+	if result then
+		table.insert(WPToolKit.events, eventName)
+    g_inputBinding:setActionEventTextVisibility(eventName, true)
+		g_inputBinding:setActionEventTextPriority(eventName, GS_PRIO_NORMAL)
+  end
+end;
+
+function WPToolKit:toggleStrongFarmer(actionName, keyStatus, arg3, arg4, arg5)
+  if actionName == "STRONGFARMER" then	
+    if WPToolKit.strongIsEnabled then 
+      Player.MAX_PICKABLE_OBJECT_MASS = WPToolKit.DEFAULT_PICKABLE_MASS;
+      WPToolKit.strongIsEnabled = false
+    else
+      Player.MAX_PICKABLE_OBJECT_MASS = 999
+      WPToolKit.strongIsEnabled = true
+    end
+  end
 end;
 
 function WPToolKit:deleteMap()
-
 end;
 
 function WPToolKit:keyEvent(unicode, sym, modifier, isDown)
-
 end;
 
 function WPToolKit:mouseEvent(posX, posY, isDown, isUp, button)
-
 end;
 
 function WPToolKit:update(dt)
 
   -- IF IT IS 8PM AND THE MOD IS NOT RUNNING
-  if not self.isRunning then
+  if not self.speedForwardIsRunning then
     if g_currentMission.environment.currentHour == 20 then
         
       -- START THE MOD
-      self.isRunning = true
+      self.speedForwardIsRunning = true
       self.speedForward()
   
     end;
   -- IF IT IS 6AM AND THE MOD IS RUNNING
-  elseif self.isRunning and g_currentMission.environment.currentHour == 6 then
+  elseif self.speedForwardIsRunning and g_currentMission.environment.currentHour == 6 then
 
     -- STOP THE MOD
     self.speedStop()
-    self.isRunning = false
+    self.speedForwardIsRunning = false
 
   end;
 end;
 
 function WPToolKit:draw()
-
 end;
 
 function WPToolKit:keyEvent(unicode, sym, modifier, isDown)
-    if isDown and sym == Input.KEY_r and Input.isKeyPressed(Input.KEY_lctrl) then
-        if not WPToolKit.isSpeeding then
-            WPToolKit.isSpeeding = true;
-            WPToolKit.lastTime = g_currentMission.missionInfo.timeScale
-			g_currentMission.missionInfo.timeScale = 6000;
-        end;
-    end;
-    
-    if not isDown and sym == Input.KEY_r and Input.isKeyPressed(Input.KEY_lctrl) then
-        if WPToolKit.isSpeeding then
-            WPToolKit.isSpeeding = false;
-            g_currentMission.missionInfo.timeScale = WPToolKit.lastTime;
-        end;
-    end;
 end;
 
 function WPToolKit:speedForward()
-
-  renderText(500.0, 500.0, 15.0, "Test Text")
 
     -- SAVE PREVIOUS TIME SCALE
     WPToolKit.lastTimeScale = g_currentMission.missionInfo.timeScale
@@ -91,5 +103,24 @@ function WPToolKit:speedStop()
     -- LOWER THE PLANT GROWTH AND UP THE TIME SCALE
     g_currentMission.missionInfo.plantGrowthRate = WPToolKit.lastGrowthScale
     g_currentMission.missionInfo.timeScale = WPToolKit.lastTimeScale
+
+end;
+
+function debugTableLogOutput(table, seperator, depth, maxDepth)
+
+  seperator = seperator or "."
+  depth = depth or 0
+  maxDepth = maxDepth or 5
+
+  if maxDepth == depth then
+    return
+  end
+
+  for i,j in pairs(table) do
+    print("[WPToolKit][debugTableLogOutput] "..seperator..tostring(i).." :: "..tostring(j))
+    if type(j) == "table" then
+      debugTableLogOutput(j, seperator..".", depth + 1, maxDepth)
+    end
+  end
 
 end;
